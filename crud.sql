9dfae6ea3b4c8a822a3c7aa7072c9040640d6267
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-01-16 13:24:25 +0530

    Add files via upload

---
+++ crud.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `crud` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
